d2f8b43390490b8558fbc565a3e509a4165fbe2c
Ritam Gupta <ritamg123@gmail.com>
2019-09-16 04:30:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+cd blockhain/workspace/raft
+sudo apt-get update
+sudo apt-get install libdb-dev libleveldb-dev libsodium-dev zlib1g-dev libtinfo-dev -y 
+screen -d -m -S constellationNode bash -c './constellation-node constellation1.conf' 
+screen -d -m -S quorumNode bash -c 'PRIVATE_CONFIG=constellation1.conf ./geth --datadir qdata/node1 --port 23000 --raftport 21000 --raft --ipcpath "./geth.ipc"' 
+screen -d -m -S gethNode 
