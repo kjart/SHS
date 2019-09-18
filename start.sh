b41a9fa113af5e77c034f4e934c305d05c2ea154
Ubuntu <ubuntu@ip-172-31-16-99.ap-south-1.compute.internal>
2019-09-18 10:00:28 +0000

    script changes

--- start.sh
+++ start.sh
@@ -6,7 +6,6 @@ sudo apt-get install libdb-dev libleveldb-dev libsodium-dev zlib1g-dev libtinfo-
 cd /usr/lib/x86_64-linux-gnu
 sudo mv libsodium.so ./libsodium.so.18
 cd /home/ubuntu/quorumTemplate/blockhain/workspace/raft
-screen -d -m -S constellationNode bash -c './constellation-node constellation1.conf' 
-cd /home/ubuntu/quorumTemplate/blockhain/workspace/raft
-screen -d -m -S quorumNode bash -c 'PRIVATE_CONFIG=constellation1.conf ./geth --datadir qdata/node1 --port 23000 --raftport 21000 --raft --ipcpath ./geth.ipc' 
-screen -d -m -S gethNode 
+screen -d -m -S constellationNode bash -c './constellation-node constellation1.conf'
+screen -d -m -S gethNode
+screen -d -m -S quorumNode bash -c 'PRIVATE_CONFIG=constellation1.conf ./geth --datadir qdata/node1 --port 23000 --raftport 21000 --raft --ipcpath ./geth.ipc'  
