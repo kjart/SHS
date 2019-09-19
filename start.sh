7015178761804369cdb7e1f73c510d4ba7ef6881
Ubuntu <ubuntu@ip-172-31-23-136.ap-south-1.compute.internal>
2019-09-19 10:58:22 +0000

    start.sh modified

--- start.sh
+++ start.sh
@@ -7,6 +7,6 @@ cd /usr/lib/x86_64-linux-gnu
 sudo mv libsodium.so ./libsodium.so.18
 cd /home/ubuntu/quorumTemplate/blockhain/workspace/raft
 screen -d -m -S constellationNode bash -c './constellation-node constellation1.conf'
+cd /home/ubuntu/quorumTemplate/blockhain/workspace/raft
 screen -d -m -S quorumNode bash -c 'PRIVATE_CONFIG=constellation1.conf ./geth --datadir qdata/node1 --port 23000 --raftport 21000 --raft --ipcpath ./geth.ipc'
-screen -d -m -S gethNode
-cd /home/ubuntu/quorumTemplate/blockhain/workspace/raft 
+screen -d -m -S gethNode 
